18d75f4330efa08384fff32e3d99226c2cef11e5
Thach Nguyen Van <83808551+notepower2k1@users.noreply.github.com>
2023-01-06 23:56:48 +0700

    Update rootInstance.js

--- src/services/utilsService/rootInstance.js
+++ src/services/utilsService/rootInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://social-network-production.up.railway.app/api",
     headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Headers": "*",
