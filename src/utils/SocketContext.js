1398bcd87adad8dcfaa2f51b1b7a7f3e8be00e1d
Thach Nguyen Van <83808551+notepower2k1@users.noreply.github.com>
2023-01-07 00:45:58 +0700

    Update SocketContext.js

--- src/utils/SocketContext.js
+++ src/utils/SocketContext.js
@@ -1,7 +1,7 @@
 import React, { createContext } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('https://socket-chat-delta.vercel.app'),
+const socket = io('http://localhost:8900'),
     SocketContext = createContext(socket);
 
 socket.on('connect', () => console.log('connected to socket'));
